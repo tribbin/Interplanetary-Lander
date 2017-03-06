7810d8db0ff0bd7646ecf07b6bafa1ae422eef5e
Robin van Westrenen-Broekmans <robin@w530.tribbin.nl>
2017-03-06 01:53:44 +0100

    Added pink spacecraft.

---
+++ objects/vehicles/spacecraft2/vehicle.js
@@ -0,0 +1,10 @@
+vehicle.width = 16;
+vehicle.height = 16;
+vehicle.weight = 1;
+vehicle.power = 1;
+vehicle.img.path = "./objects/vehicles/spacecraft2/img/"
+vehicle.img.forward = "spacecraft2_forward.svg";
+vehicle.img.left = "spacecraft2_left.svg";
+vehicle.img.right = "spacecraft2_right.svg";
+vehicle.img.landing = "spacecraft2_landing.svg";
+vehicle.translate
